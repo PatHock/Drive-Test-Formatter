084b06423f67c2840faad4324d0b779750ff6c07
Matt Jones <mattjones@workgroup.wwstereo.com>
2016-07-26 17:27:04 -0400

    debugging

--- Drive-Test-Formatter/Drive-Test-Formatter/DriveTestData.cs
+++ Drive-Test-Formatter/Drive-Test-Formatter/DriveTestData.cs
@@ -65,14 +65,14 @@ namespace Drive_Test_Formatter
             // Adds the actual data for each CSV
             for (int i=0; i<XmlNodes.Count; i++)
             {
-                //TODO: write dank regex 
+                //TODO: write dank regex
                 csvOutputs[XmlNodes[i].fileIndex] += (Environment.NewLine + Regex.Match(XmlNodes[i].Datapoint.Nodes().ToList()[0].ToString(), @"-?\d{1,3}\.\d{0,8}") + "," +
                      Environment.NewLine + Regex.Match(XmlNodes[i].Datapoint.Nodes().ToList()[1].ToString(), @"-?\d{1,3}\.\d{0,8}") + "," +
                      Environment.NewLine + Regex.Match(XmlNodes[i].Datapoint.Nodes().ToList()[2].ToString(), @"\d{1,2}:\d{2}:\d{2}") + "," +
                      Environment.NewLine + Regex.Match(XmlNodes[i].Datapoint.Nodes().ToList()[3].ToString(), @"\d{3,4}") + "," +
                      Environment.NewLine + Regex.Match(XmlNodes[i].Datapoint.Nodes().ToList()[4].ToString(), @"-?\d{1,3}\.\d{1,4}"));
             }
-            
+
             return csvOutputs;
 }
 
